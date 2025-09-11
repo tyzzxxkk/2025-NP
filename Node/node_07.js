ecb551870470b63744a335d28a8449f0e0cc90c8
태연 <s2401@e-mirim.hs.kr>
2025-09-11 11:27:46 +0900

    node

---
+++ Node/node_07.js
@@ -0,0 +1,15 @@
+// 연습문제1
+// const arr1 = [10, 20, 30];
+// const arr2 = [10, 20, 30, 40, 50];
+
+// console.log(arr1[1]); // 20
+// console.log(arr2[1]); // 20
+// console.log(arr2[4]); // 50 (배열은 0부터 시작하므로 5번째 요소는 인덱스 4)
+
+// 연습문제2
+// let isAdmin = false;
+// // let isAdmin = true;
+
+// if (isAdmin) {
+//   console.log("관리자 실행");
+// }
